5e2fbdb0fa575aa540b4781e1af7b2ea7b7687e3
antniocarlod <antniocarlod@outlook.com>
2021-09-14 23:32:36 -0300

    Cria logica branch and bound

--- Main.py
+++ Main.py
@@ -13,14 +13,14 @@ def optionsMenu():
     print('4 - Finalizar')
 
 def InsertValuesMatriz(matriz):
-    data:list = matriz
-    count:int = 0
+    data = matriz
+    count = 0
     for i in range(len(matriz)):
         for j in range(len(matriz[i])):
             count += 1
             valid = False
             while not valid:
-                result:str = input(f'Insira o {count}° valor: ').strip()
+                result = input(f'Insira o {count}° valor: ').strip()
                 if re.match('^[0-9]*$', result):
                     data[i][j] = int(result)
                     valid = True
@@ -32,13 +32,13 @@ def InsertValuesMatriz(matriz):
 def defineMatriz():
     print('------------------- Definindo Matriz -------------------')
 
-    matriz:list = []
-    valid:bool = False
+    matriz = []
+    valid = False
     while not valid:
         cls()
         try:
-            line:int = int(input('Insira a quantidade de linhas: ').strip())
-            column:int = int(input('Insira a quantidade de colunas: ').strip())
+            line = int(input('Insira a quantidade de linhas: ').strip())
+            column = int(input('Insira a quantidade de colunas: ').strip())
 
             if line and column:
                 matriz = [[0 for x in range(line)] for y in range(column)]
@@ -49,16 +49,63 @@ def defineMatriz():
     return matriz
 
 def searchBestWay(matriz):
-    pass
+    matriz = []
+    matriz.append([0,6,0,0,0,0])
+    matriz.append([0,0,53,0,5,0])
+    matriz.append([0,3,0,2,0,0])
+    matriz.append([0,0,0,0,0,0])
+    matriz.append([0,0,0,5,0,5])
+    matriz.append([0,6,0,0,0,1])
+
+    start = int(input('Start: ').strip())
+    goal = int(input('Goal: ').strip())
+    solucao = list()
+    custo = 0
+
+    solucao = branch_and_bound(matriz, start, goal, [start], solucao)
+
+    print(solucao)
+    print(custo)
+
+
+
+def branch_and_bound(matriz, node, goal, solucao_aux, solucao):
+    print(matriz[node])
+    for idx, adj in enumerate(matriz[node]):
+        if idx == node or adj == 0 or solucao_aux.count(idx) > 0 or solucao_aux.count(goal) > 0:
+            continue
+
+        if idx == goal:
+            if  calcula_custo(solucao_aux, matriz) < calcula_custo(solucao, matriz) or calcula_custo(solucao, matriz) == 0:
+                solucao = solucao_aux
+                solucao.append(idx)
+        else:
+            print(f"Adjacencias do vertice: {node}")
+            solucao_aux.append(idx)
+            print(solucao_aux)
+           # print(f"Custo: {calcula_custo(solucao_aux, matriz)}")
+           # print(f"Custo sol: {calcula_custo(solucao, matriz)}")
+            if calcula_custo(solucao_aux, matriz) < calcula_custo(solucao, matriz) or calcula_custo(solucao, matriz) == 0:
+                solucao = branch_and_bound(matriz, idx, goal, solucao_aux.copy(), solucao)
+            else:
+                solucao_aux.pop()
+    print("foi")
+    return solucao
+
+def calcula_custo(solucao, matriz):
+    soma = 0
+    for i in range(0, len(solucao)-2):
+        soma += matriz[solucao[i]][solucao[i+1]]
+    return soma
 
 def menu():
     try:
-        close:bool = False
-        matriz:list = []
+        close = False
+        matriz = []
         while close != True:
             optionsMenu()
 
-            result:int = int(input('Opção: ').strip())
+            result = int(input('Opção: ').strip())
 
             if result == 1:
                 cls()
